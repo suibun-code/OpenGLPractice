0d0a81cbe8f3c47c3d9589c377d3e1eb74e79807
Ramin Amiri <zeuspsn.55555@gmail.com>
2021-05-25 04:14:37 -0400

    1hr 3min 46sec

--- Main.cpp
+++ Main.cpp
@@ -18,30 +18,23 @@
 
 GLfloat vertices[] =
 {
-//		  POSITION	   ||      COLOR       ||   TEXTURE
-	-0.5f, -0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   0.0f, 0.0f, //Lower left corner.
-	-0.5f,  0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   0.0f, 1.0f, //Lower right corner.
-	 0.5f,  0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   1.0f, 1.0f, //Upper corner.
-	 0.5f, -0.5f, 0.0f,   1.0f, 1.0f, 1.0f,   1.0f, 0.0f, //Inner left corner.
-	
- //  -0.5f, -0.5f * float(sqrt(3)) / 3,     0.0f,   0.8f, 0.3f, 0.02f, //Lower left corner.
-	//0.5f, -0.5f * float(sqrt(3)) / 3,     0.0f,   0.8f, 0.3f, 0.02f, //Lower right corner.
-	//0.0f,  0.5f * float(sqrt(3)) * 2 / 3, 0.0f,   1.0f, 0.6f, 0.32f, //Upper corner.
-
- //  -0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,    0.9f, 0.3f, 0.17f, //Inner left corner.
-	//0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,    0.9f, 0.3f, 0.17f, //Inner right corner.
-	//0.0f,    -0.5f * float(sqrt(3)) / 3, 0.0f,    0.8f, 0.3f, 0.02f  //Inner down corner.
+//	      POSITION	    ||       COLOR        ||   TEXTURE
+	-0.5f, 0.0f,  0.5f,   0.83f, 0.70f, 0.44f,   0.0f, 0.0f,
+	-0.5f, 0.0f, -0.5f,   0.83f, 0.70f, 0.44f,   5.0f, 0.0f,
+	 0.5f, 0.0f, -0.5f,   0.83f, 0.70f, 0.44f,   0.0f, 0.0f,
+	 0.5f, 0.0f,  0.5f,   0.83f, 0.70f, 0.44f,   5.0f, 0.0f,
+	 0.0f, 0.8f,  0.0f,   0.92f, 0.86f, 0.76f,   2.5f, 5.0f
 
 };
 
 GLuint indices[] =
 {
-	0, 2, 1, //Upper triangle.
-	0, 3, 2  //Lower triangle.
-	
-	//0, 3, 5, //Lower left triangle.
-	//3, 2, 4, //Lower right triangle.
-	//5, 4, 1 //Upper triangle.
+	0, 1, 2,
+	0, 2, 3,
+	0, 1, 4,
+	1, 2, 4,
+	2, 3, 4,
+	3, 0, 4
 };
 
 int main()
@@ -76,7 +69,7 @@ int main()
 
 	VBO vbo1(vertices, sizeof(vertices));
 	IBO ibo1(indices, sizeof(indices));
-	
+
 	vao1.LinkAttrib(vbo1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
 	vao1.LinkAttrib(vbo1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
 	vao1.LinkAttrib(vbo1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
@@ -87,21 +80,49 @@ int main()
 	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
 
 	//Texture
-	Texture shrek("shrek.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
+	Texture shrek("brick.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
 	shrek.TexUnit(shaderProgram, "tex0", 0);
 
+	float rotation = 0.f;
+	double prevTime = glfwGetTime();
+
+	glEnable(GL_DEPTH_TEST);
+
 	//Loop to poll events.
-	while(!glfwWindowShouldClose(window))
+	while (!glfwWindowShouldClose(window))
 	{
 		glClearColor(0.07, 0.13f, 0.17f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		shaderProgram.Activate();
-		glUniform1f(uniID, 1.5f);
+
+		double crntTime = glfwGetTime();
+		if (crntTime - prevTime >= 1 / 165)
+		{
+			rotation += 0.2f;
+			prevTime = crntTime;
+		}
+
+		glm::mat4 model = glm::mat4(1.0f);
+		glm::mat4 view = glm::mat4(1.0f);
+		glm::mat4 projection = glm::mat4(1.0f);
+
+		model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.f, 1.f, 0.f));
+		view = glm::translate(view, glm::vec3(0.f, -0.5f, -2.0f));
+		projection = glm::perspective(glm::radians(45.0f), (float)(WINDOW_WIDTH / WINDOW_HEIGHT), 0.1f, 100.f);
+
+		int modelLoc = glGetUniformLocation(shaderProgram.ID, "model");
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		int viewLoc = glGetUniformLocation(shaderProgram.ID, "view");
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		int projectionLoc = glGetUniformLocation(shaderProgram.ID, "projection");
+		glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+		glUniform1f(uniID, 1.f);
 		shrek.Bind();
 		vao1.Bind();
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, sizeof(indices)/sizeof(int), GL_UNSIGNED_INT, 0);
 		glfwSwapBuffers(window);
-		
+
 		glfwPollEvents();
 	}
 
